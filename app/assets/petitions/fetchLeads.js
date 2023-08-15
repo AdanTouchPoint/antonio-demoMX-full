ea2f4512bd2f57cd1242e8986a11281d0e8a4e64
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-08-15 17:05:42 -0500

    fix leads endpoint

--- app/assets/petitions/fetchLeads.js
+++ app/assets/petitions/fetchLeads.js
@@ -1,15 +1,44 @@
-import { fetchData } from "./fetchData"
-const fetchLeads = (successResponse, backendURLBase, endpoints, clientId, dataUser, emailData) => {
-        
-    fetchData('POST', backendURLBase, endpoints.toSaveLeads, clientId,`&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.zipCode ? dataUser.zipCode:dataUser.state}&emailData=${dataUser.emailUser}&representative=${emailData.name}&subject=${emailData.subject}&emailMessage=${dataUser.text}&city=${emailData.state}&party=${emailData.party}&sended=${successResponse}`)
-    // axios.post(`https://payload-demo-tpm.herokuapp.com/leads?&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.zipCode ? dataUser.zipCode:dataUser.state}&emailData=${dataUser.emailUser}&representative=${emailData.name}&emailMessage=${dataUser.text}&city=${emailData.state}&party=${emailData.party}&clientId=${clientId}&sended=${respuestaDeExito}`, dataUseryEmail)
-}
-const fetchAllLeads = async (petitionMethod,backendURLBase, endpoint, clientId, setLeads) => {
-   const  leads = await fetchData(petitionMethod, backendURLBase, endpoint, clientId)
-    const data = leads.data
-    setLeads(data)
-    console.log(data)
-}
-export {
-    fetchLeads, fetchAllLeads
-}
+import { fetchData } from "./fetchData";
+import { urlEncode } from "../helpers/utilities";
+
+const fetchLeads = (
+  successResponse,
+  backendURLBase,
+  endpoints,
+  clientId,
+  dataUser,
+  emailData
+) => {
+  fetchData(
+    "POST",
+    backendURLBase,
+    endpoints.toSaveLeads,
+    clientId,
+    `&firstName=${urlEncode(dataUser.userName )}&postalcode=${
+      dataUser.postalCode ? urlEncode(dataUser.postalCode) : "NA"
+    }&emailData=${urlEncode(dataUser.emailUser)}&representative=${
+        urlEncode(emailData.name)
+    }&subject=${urlEncode(emailData.subject)}&emailMessage=${urlEncode(dataUser.text)}&city=${
+      emailData.state
+    }&party=${urlEncode(emailData.party)}&sended=${urlEncode(successResponse)}`
+  );
+  // axios.post(`https://payload-demo-tpm.herokuapp.com/leads?&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.zipCode ? dataUser.zipCode:dataUser.state}&emailData=${dataUser.emailUser}&representative=${emailData.name}&emailMessage=${dataUser.text}&city=${emailData.state}&party=${emailData.party}&clientId=${clientId}&sended=${respuestaDeExito}`, dataUseryEmail)
+};
+const fetchAllLeads = async (
+  petitionMethod,
+  backendURLBase,
+  endpoint,
+  clientId,
+  setLeads
+) => {
+  const leads = await fetchData(
+    petitionMethod,
+    backendURLBase,
+    endpoint,
+    clientId
+  );
+  const data = leads.data;
+  setLeads(data);
+  console.log(data);
+};
+export { fetchLeads, fetchAllLeads };
